535cfe199e3565c045d707366ff46186c5c095da
Oleg Frolov <frolololeg@gmail.com>
2023-06-06 22:12:22 +0100

    Initial Commit

---
+++ Difficulty Mode Selector/Difficulty_Mode_SelectorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Difficulty_Mode_SelectorApp.swift
+//  Difficulty Mode Selector
+//
+//  Created by Oleg Frolov on 06/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct Difficulty_Mode_SelectorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
